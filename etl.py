fb9b47c70ee01e170ca398bdfb847b7e679c4a25
Hasnain <Hasnain56@gmail.com>
2026-01-13 17:39:58 +0500

    Automating AQI ETL Pipeline

--- etl.py
+++ etl.py
@@ -37,7 +37,7 @@ def load_data(df_dict):
      primary_key=["aqi"],
      online_enabled=False,
      description="Air Quality Index data")
-    fg.insert(df,write_options={'kafka':False})
+    fg.insert(df)
 
 extract=extract_data()
 transform=transofrm_data(extract)
